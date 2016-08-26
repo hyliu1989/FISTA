283a97806a7f9175a0f79fc615ee58b60557db03
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2016-08-26 11:11:54 -0700

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/path/to/your/python
+# The above line is usually  #!/usr/bin/python
+
+from setuptools import setup
+
+setup( name             = 'generic FISTA'
+     , version          = '1.0.0'
+     , description      = 'implementation of FISTA algorithms'
+     , keywords         = [ 'accelerated gradient descent', 'fast gradient descent']
+     , author           = 'Hsiou-Yuan Liu'
+     , author_email     = 'hyliu@eecs.berkeley.edu'
+     , license          = 'BSD'
+     , packages         = ['genericFISTA']
+     )
