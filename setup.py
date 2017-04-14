09d50d7a9a07914c395b09d21f12f9195f25b305
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2017-04-14 14:13:11 -0700

    trim redundant structure

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-#!/path/to/your/python
-# The above line is usually  #!/usr/bin/python
-
 from setuptools import setup
 
 setup( name             = 'generic FISTA'
-     , version          = '1.3.0'
+     , version          = '1.3.1'
      , description      = 'implementation of FISTA algorithms'
      , keywords         = [ 'accelerated gradient descent', 'fast gradient descent']
      , author           = 'Hsiou-Yuan Liu'
      , author_email     = 'hyliu@eecs.berkeley.edu'
      , license          = 'BSD'
-     , packages         = ['genericFISTA']
+     , py_modules       = ['genericFISTA']
      )
