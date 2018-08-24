7209f801a5fa6a5ff97da263e10ac1d944c886f7
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2018-08-23 21:52:00 -0400

    change the way to record version

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+# import version
+with open('genericFISTA/_version.py','r') as f:
+    exec(f.read())
+assert '__version__' in locals()
+
 setup( name             = 'generic FISTA'
-     , version          = '1.3.2'
+     , version          = __version__
      , description      = 'implementation of FISTA algorithms'
      , keywords         = [ 'accelerated gradient descent', 'fast gradient descent']
      , author           = 'Hsiou-Yuan Liu'
